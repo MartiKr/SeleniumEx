659523901efcd482c8d324312bc36a8e41e4501e
Marta Krogulec <martaa.krogulec@gmail.com>
2020-08-16 14:49:23 +0200

    Add exercise 3

--- dropdownlist.py
+++ dropdownlist.py
@@ -4,6 +4,8 @@ import time
 from selenium import webdriver
 
 # expected result ==> assertion
+from selenium.webdriver.support.select import Select
+
 result = "OK. Good answer"
 
 path_d = os.getcwd() + r'\chromedriver.exe'
@@ -14,4 +16,22 @@ wd = webdriver.Chrome(executable_path=path_d)
 wd.get(url)
 
 # text to fill in
-text_to_fill = wd.find_element_by_xpath("/html/body/div/table/tbody/tr[2]/td[2]/code").text
+text_to_select = wd.find_element_by_xpath("/html/body/div/table/tbody/tr[2]/td[2]/code").text
+
+
+# select proper option
+dropdown = wd.find_element_by_xpath('//*[@id="s13"]')
+dropdown_select = Select(dropdown)
+dropdown_select.select_by_visible_text(text_to_select)
+time.sleep(1)
+
+# click: 'check solution'
+button = wd.find_element_by_id("solution").click()
+
+# chceck the answer and quit browser
+try:
+    time.sleep(1)
+    answer = wd.find_element_by_xpath('//*[@id="trail"]/code').text
+    assert answer == result
+finally:
+    wd.quit()
